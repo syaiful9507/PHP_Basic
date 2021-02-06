5778fb2aca10e0c88493ec8b871336041a9a3f1a
syaiful9507 <syaiful9507@gmail.com>
2021-02-07 00:44:14 +0700

    VARIABLE INT, FLOAT AND STRING

---
+++ Main.php
@@ -0,0 +1,26 @@
+<?php
+echo PHP_INT_MAX;
+echo "</br>";
+echo "==========VARIABEL INTEGER=============";
+echo "</br>";
+$my_var = 1;
+echo $my_var;
+
+
+
+echo "</br>";
+echo "==========VARIABEL FLOAT=============";
+echo "</br>";
+$my_var = 3.14;
+echo $my_var;
+
+
+echo "</br>";
+echo "===========VARIABEL STRING============";
+echo "</br>";
+$my_var = "SYAIFUL";
+echo $my_var;
+
+?>
+
+
